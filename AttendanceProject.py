7a6c994ff711e9a8736a499fe58f39a76a76df1e
Samantha Chan <sam.chanWT@gmail.com>
2021-03-27 16:36:22 +1300

    labelling unknown faces

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -56,14 +56,27 @@ while True:
         print(faceDis)
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        ## labelling unknown faces
+        if faceDis[matchIndex] < 0.5:
             name = classNames[matchIndex].upper()
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # rescale resulting bounding box
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
             markAttendance(name)
+        else:
+            name = "Unknown"
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # rescale resulting bounding box
+        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+        ## labelling known faces in set
+        # if matches[matchIndex]:
+        #     name = classNames[matchIndex].upper()
+        #     y1,x2,y2,x1 = faceLoc
+        #     y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # rescale resulting bounding box
+        #     cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        #     cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        #     cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+        #     markAttendance(name)
 
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
