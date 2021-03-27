b40db6e77c2a2030c32c35d37c697f6a9f31e860
Samantha Chan <sam.chanWT@gmail.com>
2021-03-27 16:25:14 +1300

    attendance taking from live feed

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -63,6 +63,7 @@ while True:
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+            markAttendance(name)
 
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
