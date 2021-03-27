3f4ed0a2616259d69a9fe9aa390b75af67d81e9a
Samantha Chan <sam.chanWT@gmail.com>
2021-03-27 16:21:59 +1300

    attendance csv and recording

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 ## Encode Known List of Images
 path = 'ImagesAttendance'
@@ -22,6 +23,19 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -30,7 +44,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.25, 0.25)
+    imgS = cv2.resize(img,(0,0),None,0.25, 0.25) # scale down image size by 4
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurFrame = face_recognition.face_locations(imgS)
@@ -39,4 +53,16 @@ while True:
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace) #lowest distance would be the best match
+        print(faceDis)
         matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4 # rescale resulting bounding box
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
